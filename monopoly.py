b8175fca0cf5d700c7be65a6044e3e64996bb30e
SteephenG <steephengarcia.123@gmail.com>
2024-10-21 16:22:35 +0200

    Comit1

--- monopoly.py
+++ monopoly.py
@@ -1,8 +1,10 @@
-#Orden de tirada
+# Orden de tirada
 import random
-#Definimos los jugadores: V (Vermell), G (Groc), T (Taronja), B (Blau)
+
+# Definimos los jugadores: V (Vermell), G (Groc), T (Taronja), B (Blau)
 jugadores = ['V', 'G', 'T', 'B']
-#Mezclamos aleatoriamente el orden de los jugadores
+
+# Mezclamos aleatoriamente el orden de los jugadores
 random.shuffle(jugadores)
 
 def tablero_monopoly():
@@ -19,7 +21,6 @@ def tablero_monopoly():
         vacio = "|" + "         |" * len(casillas)
         return f"{arriba}\n{medio}\n{vacio}\n{arriba}"
         
-    
     # Imprimir la fila superior
     print(fila(fila_arriba))
 
@@ -30,19 +31,27 @@ def tablero_monopoly():
         print(f"|         |{' ' * 49}|         |")
         print(f"+---------+{' ' * 49}+---------+")
 
+    # Modificar la casilla "Sortida" para que tenga los jugadores en una línea y "Sortida" en la siguiente
+    jugadores_concat = "".join(jugadores)  # Concatenamos los jugadores
+    fila_abajo[-1] = " " * 9  # Crear una casilla vacía
+
+    # Imprimir la fila inferior con la casilla "Sortida" en el formato adecuado
+    arriba = "+---------+---------+---------+---------+---------+---------+---------+"
+    print(arriba)
     
-
-    # Modificar la casilla "Sortida" en fila_abajo para incluir el orden de los jugadores
-    fila_abajo[-1] = f'Sortida: {"".join(jugadores)}'  # Cambiamos la última posición (Sortida)
+    # Imprimir los nombres de los jugadores en la primera línea
+    medio = "".join([f"|{casilla:^9}" for casilla in fila_abajo[:-1]]) + f"|{jugadores_concat:^9}|"
+    print(medio)
     
-    # Imprimir la fila inferior
-    print(fila(fila_abajo))
+    # Imprimir la palabra "Sortida" en la segunda línea
+    vacio = "|" + "         |" * (len(fila_abajo) - 1) + f"{'Sortida':^9}|"
+    print(vacio)
+    print(arriba)
 
-
-# Aqui lo que se ha hecho es llamara a la funcion anterior
+# Aqui lo que se ha hecho es llamar a la funcion anterior
 tablero_monopoly()
 
-#Banco del juego del Monopoly
+# Banco del juego del Monopoly
 def banco(cap_ini, cap_actual):
     # Capital inicial
     cap_actual = cap_ini
@@ -57,16 +66,16 @@ def banco(cap_ini, cap_actual):
     else:
         print(f'El capital actual es suficiente: {cap_actual}€')
 
-
-
-
-
-#Mostramos el orden de tirada y el formato de salida
+# Mostramos el orden de tirada y el formato de salida
 print(f"Significa que primer tira el {jugadores[0]}")
 print(f"després el {jugadores[1]}, després el {jugadores[2]}")
 print(f"finalment el {jugadores[3]}")
 
-#Capital inicial de cada jugador
+# Capital inicial de cada jugador
 dinero_inicial = {}
 for jugador in jugadores:
     dinero_inicial[jugador] = 2000
+
+# Tirada del Primer jugador:
+def interacciones_jugadores():
+    Tirada_primer_jugador = input(print(f'Juga "{jugadores [0]} opcions: passar"'))
