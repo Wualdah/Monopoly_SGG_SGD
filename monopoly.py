9bd3422912406f3611e11969ebf9f2211b67a3b5
SteephenG <steephengarcia.123@gmail.com>
2024-10-21 15:30:41 +0200

    Update monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -1,6 +1,13 @@
+#Orden de tirada
+import random
+#Definimos los jugadores: V (Vermell), G (Groc), T (Taronja), B (Blau)
+jugadores = ['V', 'G', 'T', 'B']
+#Mezclamos aleatoriamente el orden de los jugadores
+random.shuffle(jugadores)
+
 def tablero_monopoly():
     # Definimos las casillas del tablero en el orden correcto
-    fila_arriba = ["Parking", "Urquinaona", "Fontanella", "Suerte", "Rambles", "Pl. Catalunya", "Anr pró"]
+    fila_arriba = ["Parking", "Urquinoa", "Fontan", "Sort", "Rambles", "Pl. Cat", "Anr pró"]
     fila_abajo = ["Presó", "Consell", "Marina", "Suerte", "Rosell", "Lauria", "Sortida"]
     columna_izquierda = ["Aragó", "Sant Joan", "Caixa", "Aribau", "Muntaner"]
     columna_derecha = ["Angel", "Augusta", "Caixa", "Balmes", "Gracia"]
@@ -11,7 +18,8 @@ def tablero_monopoly():
         medio = "".join([f"|{casillas:^9}" for casillas in casillas]) + "|"
         vacio = "|" + "         |" * len(casillas)
         return f"{arriba}\n{medio}\n{vacio}\n{arriba}"
-
+        
+    
     # Imprimir la fila superior
     print(fila(fila_arriba))
 
@@ -22,8 +30,14 @@ def tablero_monopoly():
         print(f"|         |{' ' * 49}|         |")
         print(f"+---------+{' ' * 49}+---------+")
 
+    
+
+    # Modificar la casilla "Sortida" en fila_abajo para incluir el orden de los jugadores
+    fila_abajo[-1] = f'Sortida: {"".join(jugadores)}'  # Cambiamos la última posición (Sortida)
+    
     # Imprimir la fila inferior
     print(fila(fila_abajo))
+
 
 # Aqui lo que se ha hecho es llamara a la funcion anterior
 tablero_monopoly()
@@ -44,18 +58,13 @@ def banco(cap_ini, cap_actual):
         print(f'El capital actual es suficiente: {cap_actual}€')
 
 
-#Orden de tirada
-import random
-#Definimos los jugadores: V (Vermell), G (Groc), T (Taronja), B (Blau)
-jugadores = ['V', 'G', 'T', 'B']
 
-#Mezclamos aleatoriamente el orden de los jugadores
-random.shuffle(jugadores)
+
+
 #Mostramos el orden de tirada y el formato de salida
-print("+--------+")
-print(f"|{''.join(jugadores)}    | Significa que primer tira el {jugadores[0]}")
-print(f"|Sortida | després el {jugadores[1]}, després el {jugadores[2]}")
-print(f"+--------+ finalment el {jugadores[3]}")
+print(f"Significa que primer tira el {jugadores[0]}")
+print(f"després el {jugadores[1]}, després el {jugadores[2]}")
+print(f"finalment el {jugadores[3]}")
 
 #Capital inicial de cada jugador
 dinero_inicial = {}
