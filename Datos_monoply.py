d6a9004931c663230b479a2cdbf83c7315f7f49e
SteephenG <steephengarcia.123@gmail.com>
2024-10-22 21:17:44 +0200

    Datos

---
+++ Datos_monoply.py
@@ -0,0 +1,239 @@
+#Nuevo tablero
+#Datos de casillas
+jugadores = {
+    'V':{
+        "Calles":[],
+        "Dinero": 2000,
+        "Especial":[],
+        "Casilla": 0
+        },
+
+    'B':{
+        "Calles":[],
+        "Dinero":2000,
+        "Especial":[],
+        "Casilla": 0
+        },
+
+    'G':{
+        "Calles":[],
+        "Dinero": 2000,
+        "Especial":[],
+        "Casilla": 0
+        },
+
+    'T':{
+        "Calles":[],
+        "Dinero": 2000,
+        "Especial":[],
+        "Casilla": 0
+        }
+}
+
+casillas = [
+    {
+        "nombre":"Sortida",
+        "jugadores":[],
+        "tipo":"especial"
+    },
+    {
+        "nombre": "Lauria",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Rosell",
+        "jugadores":[],
+        "tipo "nombre": "Rosell",
+        "jugadores":[],
+        "tipo":"calle",ietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Sort",
+        "jugadores":[]
+    }
+
+    {
+        "nombre": "Marina",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Consell",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Prisión",
+        "jugadores":[]
+
+    }
+
+    {
+        "nombre": "Muntan",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Aribau",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Caixa",
+        "jugadores":[]
+
+    }
+
+    {
+        "nombre": "S.Joan",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Aragó",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Parking",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Urqinoa",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Lauria",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Lauria",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Lauria",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+    {
+        "nombre": "Lauria",
+        "jugadores":[],
+        "tipo":"calle",
+        "propietario":"Banco",
+        "alquilar casa":0,
+        "alquilar hotel":0,
+        "comprar terreno":0,
+        "comprar casa":0,
+        "comprar hotel":0
+
+    }
+
+]
